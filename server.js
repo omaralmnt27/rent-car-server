d6285a6d8baf126dc9ce4c8675ea7b51744ca4a2
omaralmnt27 <omaralmnt@gmail.com>
2024-11-05 07:47:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const usuariosRoutes = require('./rutas/usuario'); // Asegúrate de que la ruta sea correcta
-
 const app = express();
-
-// Middleware para parsear el cuerpo de las solicitudes como JSON
+const PORT = process.env.PORT || 4000; // Puerto en el que el servidor escuchará
 app.use(express.json());
 
-// Middleware para habilitar CORS
-app.use(cors({
-  origin: 'http://192.168.100.23:3000' // Permitir solo el frontend en esta dirección
-}));
+app.use(cors());
 
-// Configuración de las rutas
+if(process.env.NODE_ENV === "production"){
+
+}
+
 app.use('/usuarios', usuariosRoutes);
 
-// Inicio del servidor
-const PORT = 4000; // Puerto en el que el servidor escuchará
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
